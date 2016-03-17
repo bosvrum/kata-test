95685f0907ff621a55a2b489d8e3824af5d2e539
Catalin Boruga <catalinboruga@yahoo.com>
2016-03-17 19:21:25 +0100

    Add seed and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Language.create!(name: 'Ruby')
-Language.create!(name: 'Java')
-Language.create!(name: 'Python')
+
+
+ruby, java, python = ['ruby', 'java', 'python'].map do |name|
+  Language.where(name: name).first_or_create!
+end
+
+['rails','sinatra'].each do |name|
+  ruby.frameworks.where(name: name).first_or_create!
+end
+
+['django','django2'].each do |name|
+  python.frameworks.where(name: name).first_or_create!
+end
+['Spring'].each do |name|
+  java.frameworks.where(name: name).first_or_create!
+end
