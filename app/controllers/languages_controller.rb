60aa10194267d08c6e2a0b4b8f7d33937a80643f
Catalin Boruga <catalinboruga@yahoo.com>
2016-03-17 19:34:44 +0100

    Add json and finish kata

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,6 +2,11 @@ class LanguagesController < ApplicationController
 
   def index
     @languages = Language.all
+    
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @languages.to_json(include: :frameworks) }
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'languages#index'
 
-  
+  resources :languages, only: [:index]
 end
